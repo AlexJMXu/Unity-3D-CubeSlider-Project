dc91377a1ff222783fa929d2f7b56a522f9842cf
Alexander Xu <ajm.xu@outlook.com>
2017-03-10 12:31:47 +0000

    Initial Commit

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+	public PlayerMovement movement; 
+
+	void OnCollisionEnter(Collision colInfo) {
+		if (colInfo.collider.CompareTag("Obstacle")) {
+			movement.enabled = false;
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Rigidbody rb;
+	public float forwardForce = 2000f;
+	public float sidewayForce = 100f;
+
+	private bool moveRight = false;
+	private bool moveLeft = false;
+
+	void Update() {
+		if (Input.GetKey("d")) moveRight = true;
+		else moveRight = false;
+
+		if (Input.GetKey("a")) moveLeft = true;
+		else moveLeft = false;
+
+	}
+
+	void FixedUpdate () {
+		rb.AddForce(0, 0, forwardForce * Time.fixedDeltaTime);
+
+		if (moveRight) rb.AddForce(sidewayForce * Time.fixedDeltaTime, 0, 0, ForceMode.VelocityChange);
+		if (moveLeft) rb.AddForce(-sidewayForce * Time.fixedDeltaTime, 0, 0, ForceMode.VelocityChange);
+	}
+}
