6c3c09eba5505102073e454ed18bd261ed0a872c
Alexander Xu <ajm.xu@outlook.com>
2017-03-19 20:47:28 +0000

    Implemented - Score UI

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+	[SerializeField] private Transform player;
+	[SerializeField] private Text score;
+
+	void Update () {
+		score.text = player.position.z.ToString("0");
+	}
+}
